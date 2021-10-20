cac852792ce49df98cc7ae8cecb18800c745d9bd
ricardo <ricardomaziero87@gmail.com>
2021-10-20 15:53:58 -0300

    AulaAngular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { TituloComponent } from './page/titulo/titulo.component';
+import { SobrenosComponent } from './page/sobrenos/sobrenos.component';
+import { ContatoComponent } from './page/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'titulo', component: TituloComponent},
+  {path: 'sobrenos', component: SobrenosComponent},
+  {path: 'contato', component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { TituloComponent } from './page/titulo/titulo.component';
+import { SobrenosComponent } from './page/sobrenos/sobrenos.component';
+import { ContatoComponent } from './page/contato/contato.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TituloComponent,
+    SobrenosComponent,
+    ContatoComponent,
   ],
   imports: [
     BrowserModule,
